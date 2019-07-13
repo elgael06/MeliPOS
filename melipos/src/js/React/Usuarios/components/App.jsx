e575bf22a07325e828833c8a461fa1b1fcb6408d
elgael06 <elgael06@gmail.com>
2019-07-12 18:54:22 -0600

    Edicion Usuarios

--- melipos/src/js/React/Usuarios/components/App.jsx
+++ melipos/src/js/React/Usuarios/components/App.jsx
@@ -10,6 +10,7 @@ let $MI_URL = `${window.location.protocol}//${window.location.hostname}`,
 const App = () => {
   const [lista, setLista] = useState([]);
   const [seleccion, setSeleccion] = useState({});
+
   const ObtenerUsuarios = () => {
     fetch(`${$URL_API}/usuarios/api/`, {
       method: "get",
@@ -25,22 +26,48 @@ const App = () => {
         })
       );
   };
-  const editar = sel => {
+
+  const editarUsuario = sel => {
     console.log("seleccion=>", sel);
-    setSeleccion(sel);
+    setSeleccion({
+      id: sel.id,
+      nombre: sel.nombre,
+      nombre_completo: sel.nombre_completo,
+      password: sel.password,
+      usuario_creo: sel.usuario_creo,
+      usuario_modifico: sel.usuario_modifico
+    });
     document.querySelector("#modal-edicion").style.display = "flex";
   };
 
+  const evUsuario = (valor, parametro) => {
+    let sel = {
+      id: seleccion.id,
+      nombre: seleccion.nombre,
+      nombre_completo: seleccion.nombre_completo,
+      password: seleccion.password,
+      usuario_creo: seleccion.usuario_creo,
+      usuario_modifico: seleccion.usuario_modifico
+    };
+    sel[parametro] = valor;
+    setSeleccion(sel);
+  };
+
   lista.length == 0 ? ObtenerUsuarios() : "";
+
   return (
     <div class="card">
       <div class="card-header">
         <h3>Usuarios</h3>
       </div>
       <div class="card-body">
-        <Usuarios lista={lista} seleccion={editar} />
+        <Usuarios lista={lista} seleccion={editarUsuario} />
       </div>
-      <Editar usuario={seleccion} />
+      <Editar
+        usuario={seleccion}
+        evento={evUsuario}
+        ObtenerUsuarios={ObtenerUsuarios}
+      />
     </div>
   );
 };
--- melipos/src/js/React/Usuarios/components/Edicion.jsx
+++ melipos/src/js/React/Usuarios/components/Edicion.jsx
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
 
-const Editar = ({ usuario }) => {
+const Editar = ({ usuario, evento, ObtenerUsuarios }) => {
   const [password, setPassword] = useState("");
   const [confirmar, setConfirmar] = useState("");
+
   const cerrar = () => {
     document.querySelector("#modal-edicion").style.display = "none";
   };
   const guardar = () => {
     console.log("Guardar...");
+    if (checarPassword) {
+      evento(password, "password");
+      enviar();
+    }
   };
+  const checarPassword = () => {
+    return password === confirmar;
+  };
+
+  async function enviar() {
+    try {
+      const response = await fetch("/usuarios/api/", {
+        method: "post",
+        credentials: "same-origin",
+        body: JSON.stringify({
+          nombre: usuario.nombre,
+          nombre_completo: usuario.nombre_completo,
+          password: password
+        }),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      });
+      await response.json().then(r => {
+        ObtenerUsuarios();
+      });
+    } catch (err) {
+      console.log("fetch failed", err);
+    }
+  }
+
   return (
     <div class="modal_base" id="modal-edicion">
       <div class="card" style={{ maxWidth: "650px" }}>
@@ -20,11 +51,19 @@ const Editar = ({ usuario }) => {
           <div class="row">
             <div class="col-sm-12">
               <label>Nombre</label>
-              <input class="form-control" value={usuario.nombre_completo} />
+              <input
+                class="form-control"
+                onChange={e => evento(e.target.value, "nombre_completo")}
+                value={usuario.nombre_completo}
+              />
             </div>
             <div class="col-sm-4">
               <label>Alias</label>
-              <input class="form-control" value={usuario.nombre} />
+              <input
+                class="form-control"
+                onChange={e => evento(e.target.value, "nombre")}
+                value={usuario.nombre}
+              />
             </div>
             <div class="col-sm-4">
               <label>Password</label>
