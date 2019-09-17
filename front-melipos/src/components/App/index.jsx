c503f8f41b3e7683bb0ce5df24cce4d24d7c7954
elgael <elgael06@gmail.com>
2019-09-17 17:47:50 -0600

    creacionde sap y estructura de caerpetas

---
+++ front-melipos/src/components/App/index.jsx
@@ -0,0 +1,12 @@
+//librerias
+import React from 'react';
+//estilos
+import './styles.css'
+
+const App=({children})=> {
+  return (<div className="App">
+      <label >page main <b>App.</b></label>
+    </div>);
+}
+
+export default App;
---
+++ front-melipos/src/components/Login/index.jsx
@@ -0,0 +1,10 @@
+//librerias
+import React from 'react';
+
+const Login =()=>{
+    return (<div className="login-container">
+        <label >this is the <b>login</b></label>
+    </div>);
+}
+
+export default Login;
